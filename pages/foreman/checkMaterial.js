de6975e4d3f39083e0fb372f4ef68f6ac09f25f4
panny <258788457@qq.com>
2017-09-14 15:46:37 +0800

    9-14
    更新更改材料报备配送人员

--- pages/foreman/checkMaterial.js
+++ pages/foreman/checkMaterial.js
@@ -10,6 +10,12 @@ Page({
   data: {
     id: 0,
     info: null,
+    editShow: true,
+    userShow: false,
+    purchase: Array(),
+    users: Array(),
+    usersChoose: Array(),
+    winHeight: app.globalData.winHeight,
   },
 
   /**
@@ -21,11 +27,19 @@ Page({
     let url = "https://xcx.envisioneer.cn/foreman/checkMaterial";
     api.request(url, { id })
       .then(function (res) {
-        res.date = util.formatUnixToDate(res.time);
-        res.time = util.formatUnixToTime(res.time);
+        let { material, purchase, users} = res;
+        material.date = util.formatUnixToDate(material.time);
+        material.time = util.formatUnixToTime(material.time);
+        let data = Array();
+        for (let i = 0; i < purchase.length; i++){
+          data[data.length] = purchase[i].uid;
+        }
         that.setData({
           id: id,
-          info: res,
+          info: material,
+          users: users,
+          usersChoose: data,
+          purchase: data,
         })
       })
   },
@@ -46,15 +60,45 @@ Page({
       info: info,
     })
   },
+  editUser: function (e) {
+    let users = this.data.users;
+    this.setData({
+      userShow: true,
+      editShow: false,
+      users: users
+    })
+  },
+  editCancl: function (e) {
+    let purchase = this.data.purchase;
+    this.setData({
+      userShow: false,
+      editShow: true,
+      usersChoose: purchase
+    })
+  },
+  usercheckboxChange: function (e) {
+    this.data.usersChoose = e.detail.value;
+
+    console.log(this.data.usersChoose)
+  },
+  userSure: function (e) {
+    let usersChoose = this.data.usersChoose;
+    this.setData({
+      userShow: false,
+      editShow: true,
+      purchase: usersChoose
+    })
+    console.log(this.data)
+  },
   back: function () {
     wx.navigateBack({
       delta: 1
     })
   },
   send: function () {
-    let { id, info } = this.data;
+    let { id, info, purchase} = this.data;
     let that = this;
-    api.request("https://xcx.envisioneer.cn/foreman/editMaterial", { id, info })
+    api.request("https://xcx.envisioneer.cn/foreman/editMaterial", { id, info, purchase})
       .then(function (res) {
         if (res == 22001) {
           wx.showModal({
