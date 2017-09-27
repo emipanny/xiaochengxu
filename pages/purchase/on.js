023e55c61e9387f8de75a3dad426485657b60e4a
panny <258788457@qq.com>
2017-09-27 17:26:05 +0800

    9-27
    初步完成配送员查看数据签到功能

--- pages/purchase/on.js
+++ pages/purchase/on.js
@@ -1,66 +1,120 @@
-// pages/purchase/on.js
+// pages/boss/checkProject.js
+const util = require('../../utils/util');
+const api = require('../../utils/api');
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    material: Array(),
+    time: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad: function (option) {
+    let time;
+    if (option.time) time = option.time;
+    else time = util.formatUnixToDUnix(Date.parse(new Date()) / 1000);
+    
+    this.init(time);
   },
+  init: function (time) {
+    wx.showLoading({
+      title: '加载中',
+    })
+    let that = this;
+    let url = "https://xcx.envisioneer.cn/purchase/getHome";
+    api.request(url, { time })
+      .then(function (res) {
+        console.log(time);
+        console.log(res);
+        let { material } = res;
+        material.forEach(function (item) {
+          item.time = util.formatUnixToTime(item.time);
+          item.arrive = util.formatUnixToTime(item.arrive);
+        });
+        that.setData({
+          time: time,
+          material: material,
+        })
+        wx.hideLoading();
+      })
+    wx.setNavigationBarTitle({
+      title: util.formatUnixToDate(time)
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  prev: function () {
+    let {time} = this.data;
+    time = time - 86400;
+    api.redirectTo("./on?time=" + time);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  next: function () {
+    let {time} = this.data;
+    time = Number(time) + 86400;
+    api.redirectTo("./on?time=" + time);
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  dateChange: function (e) {
+    let {time} = this.data;
+    let date = e.detail.value;
+    time = util.formatDateUnix(date);
+    api.redirectTo("./on?time=" + time);
   },
+  arrive: function (e) {
+    let id = e.currentTarget.dataset.id;
+    let that = this;
+    wx.showModal({
+      title: '提示',
+      content: '您已经送到该地点并打卡吗',
+      success: function (res) {
+        if (res.confirm) {
+          that.sign(id);
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
   },
+  sign: function (id) {
+    wx.showLoading({ mask: true });
+    let that = this;
+    let position;
+    api.checkLocation()
+      .then(function () {
+        return api.getLocation()
+      })
+      .then(function (res) {
+        let { latitude, longitude } = res;
+        return api.request("https://xcx.envisioneer.cn/purchase/sign", { id, latitude, longitude });
+      })
+      .then(function (res) {
+        if (res.errcode) {
+          wx.hideLoading();
+          wx.showModal({
+            title: '签到失败',
+            content: '请检查是否开启了定位，或者您不在需要打卡的区域',
+            showCancel: false,
+          })
+        }
+        else {
+          let list = that.data.material;
+          list.forEach(function (item) {
+            if (item.id == id) {
+              item.state = 1;
+              item.arrive = util.formatUnixToTime(res.arrive);
+            }
+          });
+          that.setData({
+            material: list,
+          });
+          wx.hideLoading();
+          wx.showModal({
+            title: '签到成功',
+            showCancel: false,
+          })
+        }
+      })
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
